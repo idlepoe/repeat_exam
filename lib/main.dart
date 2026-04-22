2c938ce9f5ad9e7254366e250e8391360c244d6a
jylee <idlepoe@gmail.com>
2026-04-22 14:26:42 +0900

    save

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flex_seed_scheme/flex_seed_scheme.dart';
 
 import 'package:get/get.dart';
 
@@ -13,10 +14,29 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const white = Colors.white;
+    const black = Colors.black;
+    final lightScheme = SeedColorScheme.fromSeeds(
+      brightness: Brightness.light,
+      primaryKey: const Color(0xFF111111),
+      secondaryKey: const Color(0xFF222222),
+      surface: white,
+      onSurface: black,
+    );
+
     return GetMaterialApp(
       title: "광고없는제과제빵기출회독",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: lightScheme,
+        scaffoldBackgroundColor: white,
+        textTheme: ThemeData.light().textTheme.apply(
+          bodyColor: black,
+          displayColor: black,
+        ),
+      ),
     );
   }
 }
